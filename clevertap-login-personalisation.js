e303ceb2370ef18f02304fd2190abeb52402f397
Shivam Kotak <shivamkotak16.sk@gmail.com>
2022-12-20 18:07:17 +0530

    Update clevertap-login-personalisation.js

--- clevertap-login-personalisation.js
+++ clevertap-login-personalisation.js
@@ -1,1 +1,68 @@
-if(void 0===clevertap)var clevertap={event:[],profile:[],account:[],notifications:[],onUserLogin:[],region:"sk1",privacy:[]};clevertap.account.push({id:"WRK-485-456Z"}),clevertap.privacy.push({optOut:!1}),clevertap.privacy.push({useIP:!1}),function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.jsdelivr.net/gh/CleverTap/clevertap-web-sdk/clevertap.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();const fallback=document.getElementById("fallback"),mainWrapper=document.getElementById("main-wrapper"),fallbackNode=fallback.content.cloneNode(!0);function renderLoginPage(e){console.log("event",e)}mainWrapper.appendChild(fallbackNode),clevertap.event.push("Login Page Visited"),document.addEventListener("CT_web_native_display",(function(e){const t=e.detail;if("login_personalisation"===t.kv.topic)renderLoginPage(t)}));
+if(typeof clevertap === "undefined"){
+  var clevertap = {
+    event: [],
+    profile: [],
+    account: [],
+    notifications: [],
+    onUserLogin: [],
+    region:"sk1",
+    privacy: []
+  };
+}
+
+// sk1 - Web Testing.
+clevertap.account.push({"id": "WRK-485-456Z"});
+clevertap.privacy.push({optOut: false});
+clevertap.privacy.push({useIP: false});
+
+(function () {
+  var wzrk = document.createElement("script");
+  wzrk.type = "text/javascript";
+  wzrk.async = true;
+  wzrk.src = "https://cdn.jsdelivr.net/gh/CleverTap/clevertap-web-sdk/clevertap.min.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(wzrk, s);
+})();
+
+// render fallback content
+const fallback = document.getElementById("fallback");
+const mainWrapper = document.getElementById("main-wrapper");
+const fallbackNode = fallback.content.cloneNode(true);
+mainWrapper.appendChild(fallbackNode);
+
+clevertap.event.push("Login Page Visited");
+
+function renderLoginPage (data) {
+  const mainWrapper = document.getElementById("main-wrapper");      
+  // clear fallback content
+  mainWrapper.innerHTML = "";
+  // render custom template
+  const custom = document.getElementById("custom");
+  const customNode = custom.content.cloneNode(true);
+  mainWrapper.appendChild(customNode);
+
+  mainWrapper.style.justifyContent = "space-between";
+  mainWrapper.style.backgroundColor = data.kv.backgroundColor;
+  mainWrapper.style.color = data.kv.textColor;
+  // update data from key value pairs
+  const label = document.getElementById("label");
+  const title = document.getElementById("title");
+  const desc = document.getElementById("desc");
+  const image = document.getElementById("image");
+
+  label.innerText = data.kv.label;
+  title.innerText = data.kv.title;
+  desc.innerText = data.kv.desc;
+
+  image.src = data.kv.imageUrl;
+}
+
+document.addEventListener("CT_web_native_display", function(event) {
+  const data = event.detail;
+  const topic = data.kv.topic;
+  switch (topic) {
+    case "login_personalisation":
+      renderLoginPage(data);
+      break;
+  }
+});
