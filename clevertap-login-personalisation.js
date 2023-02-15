6a089d53bf06a940e5a5131f9eae15abedb9e471
Shivam Kotak <shivamkotak16.sk@gmail.com>
2023-02-15 17:43:15 +0530

    Update clevertap-login-personalisation.js

--- clevertap-login-personalisation.js
+++ clevertap-login-personalisation.js
@@ -24,43 +24,59 @@ clevertap.privacy.push({useIP: false});
   s.parentNode.insertBefore(wzrk, s);
 })();
 
-// render fallback content
+// render loading screen
+const loadingScreen = document.getElementById("loading");
+const mainWrapper = document.getElementById("main-wrapper");
+const loadingScreenNode = loadingScreen.content.cloneNode(true);
+mainWrapper.appendChild(loadingScreenNode);
+
+var isFallbackLoaded = false
+// render fallback screen
 var fallbackTimeout = setTimeout(function () {
-  const fallback = document.getElementById("fallback");
+  isFallbackLoaded = true
   const mainWrapper = document.getElementById("main-wrapper");
+  // clear loading screen
+  mainWrapper.innerHTML = "";
+  // append fallback screen
+  const fallback = document.getElementById("fallback");
   const fallbackNode = fallback.content.cloneNode(true);
   mainWrapper.appendChild(fallbackNode);
-}, 2000);
+  // update styles
+  mainWrapper.style.backgroundColor = '#101727';
+  mainWrapper.style.color = 'white';
+}, 500);
 
 
 clevertap.event.push("Login Page Visited");
 
 function renderLoginPage (data) {
   const mainWrapper = document.getElementById("main-wrapper");      
-  // clear fallback content
+  // clear content
   mainWrapper.innerHTML = "";
   // render custom template
   const custom = document.getElementById("custom");
   const customNode = custom.content.cloneNode(true);
   mainWrapper.appendChild(customNode);
 
-  mainWrapper.style.justifyContent = "space-between";
   mainWrapper.style.backgroundColor = data.kv.backgroundColor;
   mainWrapper.style.color = data.kv.textColor;
   // update data from key value pairs
   const label = document.getElementById("label");
   const title = document.getElementById("title");
   const desc = document.getElementById("desc");
+  const cta = document.getElementById("cta");
   const image = document.getElementById("image");
 
   label.innerText = data.kv.label;
   title.innerText = data.kv.title;
   desc.innerText = data.kv.desc;
-
+  cta.innerText = data.kv.ctaText;
+  cta.href = data.kv.ctaUrl;
   image.src = data.kv.imageUrl;
 }
 
 document.addEventListener("CT_web_native_display", function(event) {
+  if (isFallbackLoaded) return
   clearTimeout(fallbackTimeout);
   const data = event.detail;
   const topic = data.kv.topic;
